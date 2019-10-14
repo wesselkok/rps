1100d4ec6ec3fad37af1d3a3fb98fc4f2fb6e338
wesselkok <wessel.pl.kok@gmail.com>
2019-10-14 11:44:02 +0200

    Mark zuigt apen dollo's

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static('public'));
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
