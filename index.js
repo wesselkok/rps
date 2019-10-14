4d83b07ba229a0ea131994bf3e0cbfd2bdb13d3f
wesselkok <wessel.pl.kok@gmail.com>
2019-10-14 11:55:08 +0200

    Hopelijk dis works

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var app = require('express')();
+const app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+const express = require('express');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
+// Static files
 app.use(express.static('public'));
 
+// Main get
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/main.html');
+});
+
+// On Connection listener
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
