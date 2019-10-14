fd527fc30e231f157325e7a9baf9be5d54c3143d
MarkSpruijt <mail@markspruijt.nl>
2019-10-14 14:07:40 +0200

    Added team player assign.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 const express = require('express');
 
+var clients = [];
+
 // Static files
 app.use(express.static('public'));
 
@@ -19,11 +21,41 @@ app.get('/', function(req, res){
 
 // On Connection listener
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    if(socket.handshake.query.name != "game"){
+        // New client connected
+        console.log('New client connected');
+        var clientId = socket.id;
+        var team = GetNewClientTeam();
+        clients.push({
+            'clientId': clientId,
+            'team': team,
+        });
+        console.log(team);
+        console.log(clientId);
+        io.to(clientId).emit('team', team);
+        socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        });
+    }
+
+    console.log(clients.length);
+    
+    // Disconnect
+    socket.on('disconnect', function() {
+        console.log('Disconnected');
+        clients.splice(clients.findIndex(x => x.clientId == socket.id), 1);
+    });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+function GetNewClientTeam(){
+    var RedCount = clients.filter(x => x.team == "red").length;
+    var BlueCount = clients.filter(x => x.team == "blue").length;
+    if (RedCount > BlueCount){
+        return 'blue';
+    }
+    return 'red';
+}
